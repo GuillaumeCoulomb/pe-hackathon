82ce9c3d23563ac7c5ca2b715b8e1b504dbeb1c2
Adrian Dabas <adrian.dabas@etu.minesparis.psl.eu>
2024-10-29 17:52:20 +0100

    ajout de la fonction frequence à common

--- common.py
+++ common.py
@@ -74,3 +74,39 @@ def zoom_japon(df):
 
 
 zoom_japon(df)
+
+
+def frequence(pts):
+    """prend en argument une dataframe, par exemple df[['Longitude', 'Latitude']]"""
+    d = pts['geometry'] = df.apply(lambda x: Point((float(x.Longitude), float(x.Latitude))), axis=1)
+    d = gpd.GeoDataFrame(pts, geometry='geometry')
+    bins=100
+    smoothing=1.5
+    cmap='hot_r'
+    
+    def getx(pt):
+        return pt.coords[0][0]
+
+    def gety(pt):
+        return pt.coords[0][1]
+
+    x = list(d.geometry.apply(getx))
+    y = list(d.geometry.apply(gety))
+    heatmap, xedges, yedges = np.histogram2d(y, x, bins=bins)
+    extent = [yedges[0], yedges[-1], xedges[-1], xedges[0]]
+
+    logheatmap = np.log(heatmap)
+    logheatmap[np.isneginf(logheatmap)] = 0
+    logheatmap = ndimage.filters.gaussian_filter(logheatmap, smoothing, mode='nearest')
+
+    world = gpd.read_file(geodatasets.get_path('naturalearth.land'))
+    world.plot(color='white', edgecolor='black')
+    plt.imshow(logheatmap, cmap=cmap, extent=extent)
+    plt.colorbar()
+    plt.gca().invert_yaxis()
+    plt.show()
+
+
+frequence(df_propre[['Longitude', 'Latitude'])
+
+
