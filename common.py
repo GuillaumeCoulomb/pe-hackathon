b6f91533360320052740b6dcdfb726dfa4705264
Tristan Chariot <tristan.chariot@etu.minesparis.psl.eu>
2024-10-29 15:55:48 +0100

    fonction supression_donnes

--- common.py
+++ common.py
@@ -1,1 +1,14 @@
 # Ceci est le fichier commun avec nos fonctions
+
+df = pd.read_csv('earthquakes.csv')
+
+
+def supression_donnees(df):
+    '''prend en argument une dataframe df contenant les données des tremblements de terre
+    modiie la dataframe pour avoir la date, l'heure, la localisation, la profondeur et la magnitude'''
+    df.set_index('ID', inplace = True)
+    df.drop(['Depth Error', 'Depth Seismic Stations', 'Magnitude Type', 'Magnitude Error', 'Magnitude Seismic Stations'], axis=1, inplace=True)
+    df.drop(['Azimuthal Gap', 'Horizontal Distance', 'Horizontal Error', 'Root Mean Square'], axis=1, inplace=True) 
+    df.drop(['Source', 'Location Source', 'Status', 'Magnitude Source', 'Type'], axis=1, inplace=True)
+
+
