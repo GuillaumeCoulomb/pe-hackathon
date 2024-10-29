7f6595cac7fe7abbcb31f18fb3e2a2fd9d461dc8
Guillaume Coulomb <guillaume.coulomb@etu.minesparis.psl.eu>
2024-10-29 17:11:15 +0100

    modif opacité

--- common.py
+++ common.py
@@ -24,6 +24,21 @@ df_propre = suppression_donnees(df)
 def magnitude(df):
     world = gpd.read_file(geodatasets.get_path("naturalearth.land"))
     world.plot(color='white', edgecolor='black')
+    plt.scatter(df[(df.Magnitude < 6) & (df.Magnitude >=5)].Longitude, df[(df.Magnitude < 6) & (df.Magnitude >=5)].Latitude, s=0.5, color='b', alpha=0.5)
+    plt.scatter(df[(df.Magnitude < 7) & (df.Magnitude >=6)].Longitude, df[(df.Magnitude < 7) & (df.Magnitude >=6)].Latitude, s=1, color='g', alpha=0.5)
+    plt.scatter(df[(df.Magnitude < 8) & (df.Magnitude >=7)].Longitude, df[(df.Magnitude < 8) & (df.Magnitude >=7)].Latitude, s=2.5, color='orange', alpha=0.5)
+    plt.scatter(df[(df.Magnitude < 10) & (df.Magnitude >=8)].Longitude, df[(df.Magnitude < 10) & (df.Magnitude >=8)].Latitude, s=5, color='red', alpha=0.5)
+    plt.xlim(-180, 180)
+    plt.ylim(-90, 90)
+    plt.show()
+
+
+magnitude(df_propre)
+
+
+def magnitude(df):
+    world = gpd.read_file(geodatasets.get_path("naturalearth.land"))
+    world.plot(color='white', edgecolor='black')
     plt.scatter(df[(df.Magnitude < 6) & (df.Magnitude >=5)].Longitude, df[(df.Magnitude < 6) & (df.Magnitude >=5)].Latitude, s=0.5, color='b')
     plt.scatter(df[(df.Magnitude < 7) & (df.Magnitude >=6)].Longitude, df[(df.Magnitude < 7) & (df.Magnitude >=6)].Latitude, s=1, color='g')
     plt.scatter(df[(df.Magnitude < 8) & (df.Magnitude >=7)].Longitude, df[(df.Magnitude < 8) & (df.Magnitude >=7)].Latitude, s=2.5, color='orange')
@@ -34,3 +49,5 @@ def magnitude(df):
 
 
 magnitude(df_propre)
+
+
