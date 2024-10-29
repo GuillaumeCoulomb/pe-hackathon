3e2bd0ebf5eb5efc68457701cb05bf8e5597d85a
Tristan Chariot <tristan.chariot@etu.minesparis.psl.eu>
2024-10-29 15:53:12 +0100

    fonction supr data

--- Notebook_Tristan.py
+++ Notebook_Tristan.py
@@ -15,12 +15,58 @@
 
 # %%
 import geopandas as gpd
-from geodatasets import get_path
+import pandas as pd
 
 # %%
-data_brut = gpd.read_file('https://public.opendatasoft.com/explore/dataset/significant-earthquake-database/export/')
+df = pd.read_csv('earthquakes.csv')
 
 # %%
-mode_emploi = gpd.read_file('https://public.opendatasoft.com/explore/dataset/significant-earthquake-database/information/')
+df.set_index('ID', inplace = True)
+df.head(10)
+
+# %%
+df.drop(['Depth Error', 'Depth Seismic Stations', 'Magnitude Type', 'Magnitude Error', 'Magnitude Seismic Stations'], axis=1, inplace=True)
+
+# %%
+df.head(10)
+
+# %%
+df.drop(['Azimuthal Gap', 'Horizontal Distance', 'Horizontal Error', 'Root Mean Square'], axis=1, inplace=True) 
+
+# %%
+df.head(5)
+
+# %%
+df.drop(['Source', 'Location Source', 'Status', 'Magnitude Source'], axis=1, inplace=True)
+
+# %%
+df.head(5)
+
+# %%
+df = df[df.Type == 'Earthquake']
+df.head(5)
+
+
+# %%
+def supression_donnees(df):
+    '''prend en argument une dataframe df contenant les données des tremblements de terre
+    modiie la dataframe pour avoir la date, l'heure, la localisation, la profondeur et la magnitude'''
+    df.set_index('ID', inplace = True)
+    df.drop(['Depth Error', 'Depth Seismic Stations', 'Magnitude Type', 'Magnitude Error', 'Magnitude Seismic Stations'], axis=1, inplace=True)
+    df.drop(['Azimuthal Gap', 'Horizontal Distance', 'Horizontal Error', 'Root Mean Square'], axis=1, inplace=True) 
+    df.drop(['Source', 'Location Source', 'Status', 'Magnitude Source'], axis=1, inplace=True)
+    # df = df[df.Type == 'Earthquake', inplace=True]
+    # df.drop(['Type'], axis=1, inplace=True)
+
+
+
+# %%
+df1 = pd.read_csv('earthquakes.csv')
+supression_donnees(df1)
+
+# %%
+df1.head(5)
+
+# %%
 
 # %%
